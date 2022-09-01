b5d34392b45e11415b6cd9beecbf933f6e1ed82d
100167684 <100167684@lubbockisd.net>
2022-09-01 08:33:36 -0500

    message updated to previous original message

--- src/MyFirstProject.java
+++ src/MyFirstProject.java
@@ -1,7 +1,7 @@
 public class MyFirstProject {
     public static void main(String[] args) {
         System.out.print("Morning.... I guess.");
-        System.out.println("Community College is completely underrated");
+        System.out.println("Community College is underrated");
 
     }
 }
