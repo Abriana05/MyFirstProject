50c56deb5f4e594cd7b089f7b3ea878ea1145b2b
Abriana Capetillo <100167684@lubbockisd.net>
2022-09-01 08:25:34 -0500

    Initial commit

---
+++ src/MyFirstProject.java
@@ -0,0 +1,7 @@
+public class MyFirstProject {
+    public static void main(String[] args) {
+        System.out.print("Morning.... I guess.");
+        System.out.println("Community College is completely underrated");
+
+    }
+}
